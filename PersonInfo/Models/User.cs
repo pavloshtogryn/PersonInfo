82ce9b7ae985c81979a32139ae21b1dae0bdf7c3
Pavlo Shtohryn <pavlo.shtohryn@apriorit.com>
2023-05-03 18:05:51 +0300

    Add project files.

---
+++ PersonInfo/Models/User.cs
@@ -0,0 +1,29 @@
+﻿using System.Text.Json.Serialization;
+using System.Xml.Linq;
+
+namespace PersonInfo.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public int Age 
+        {
+            get
+            {
+                int age = DateTime.Now.Year - DateOfBirth.Year;
+
+                if (DateTime.Now.DayOfYear > DateOfBirth.DayOfYear)
+                {
+                    return age;
+                }
+                else
+                {
+                    return --age;
+                }
+            }
+        }
+    }
+}
